7c29b2efb5210a21983f9ba57200bebde8755bf3
hansinikarunarathne <hansini.20@cse.mrt.ac.lk>
2025-04-28 12:36:38 +0530

    Changed testing

--- Res2Net/models/resnet_blocks.py
+++ Res2Net/models/resnet_blocks.py
@@ -380,5 +380,4 @@ class SEBottle2neck(nn.Module):
             if h < self.dropblock.block_size or w < self.dropblock.block_size:
                 # print("Skipping DropBlock due to spatial dimensions smaller than block size.")
                 return x  # skip DropBlock
-        print('applying dropblock')
         return self.dropblock(x)
