21ca510e27233ba69602e3fbd184fc8f704171c2
hansinikarunarathne <hansini.20@cse.mrt.ac.lk>
2025-05-14 10:24:53 +0530

    Chnaged setup.py file

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="soniccypher",
     version="0.1.0",
-    packages=find_packages(include=["Res2Net*", "Model*", "Decision_Fusion*", "Preprocessing*"]),
+    packages=find_packages(),  # include all folders with __init__.py
+    py_modules=["run_pipline"]
 )
